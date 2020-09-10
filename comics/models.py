f36a24a5a2371709bdcf70e435fcdb531721d9e3
SpideyFanDan <dsmith.teach@gmail.com>
2020-09-10 16:49:50 -0600

    confirmed publication_date and notes are not required

--- comics/models.py
+++ comics/models.py
@@ -8,8 +8,8 @@ class ComicBook(models.Model):
     issue = models.CharField(max_length=25)
     publication_date = models.CharField(max_length=100)
     cover_url = models.TextField()
-    personal_image = models.ImageField(upload_to='images/', required=False)
-    notes = models.TextField(required=False)
+    personal_image = models.ImageField(upload_to='images/', blank=True)
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
